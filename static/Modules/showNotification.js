877cfa97a1ab48d156a3350b89d97dc7e1ea70b3
HioNobi <dotrunghieu0510@gmail.com>
2020-09-04 16:46:23 +0700

    updated

--- static/Modules/showNotification.js
+++ static/Modules/showNotification.js
@@ -1,6 +1,11 @@
 const showNotification = (id, data) => {
     console.log(data)
     menu = document.getElementById("profile-notification-dropdown-menu");
+
+    if(!(data.read.includes(currentId))){
+        let text = $("#profile-notification-dropdown-btn span").text()
+        $("#profile-notification-dropdown-btn span").text(String(Number(text) + 1))
+    }
    
     menu.innerHTML += `
         <div id="profile-notification-dropdown-menu-${id}" style="border-bottom: 1px solid rgb(117, 116, 116);">
